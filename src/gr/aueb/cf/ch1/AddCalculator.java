769c521f9195d91c70170eef152734e77a9186f5
Evangelia Petraki <petrakie@icloud.com>
2024-12-16 19:01:23 +0100

    hotfix - println added

--- src/gr/aueb/cf/ch1/AddCalculator.java
+++ src/gr/aueb/cf/ch1/AddCalculator.java
@@ -9,6 +9,7 @@ public class AddCalculator {
 
         result = add(num1, num2);
 
+        System.out.println("Sum: " + result);
     }
         public static int add(int a, int b) {
         return a + b;
